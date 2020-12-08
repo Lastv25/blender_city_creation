d3669fea523ee6cd463f894560c0052fc7551e92
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-12-08 10:46:04 +0100

    project setup with right folder paths

--- addon/buttons/load_image.py
+++ addon/buttons/load_image.py
@@ -1,9 +1,10 @@
 import bpy
+from .. request_scripts.get_data import get_city_info
  
 class loadImage(bpy.types.Operator):
     bl_idname = 'custom.load_road_image'
     bl_label = 'Load Image'
  
     def execute(self, context):
-        print('Current City:', context.scene.city_name)
+        get_city_info(context.scene.city_name)
         return {"FINISHED"}
---
+++ addon/request_scripts/get_data.py
@@ -0,0 +1,4 @@
+import requests as re
+
+def get_city_info(city_name):
+    print(city_name)
